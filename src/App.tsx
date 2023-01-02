eed60667dcf2f326e762cdca5a1fd3a4680d8c8d
Juansasa <quavun@gmail.com>
2023-01-02 12:49:50 +0100

    Added marked dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Page from "./components/page";
 import Divider from "./components/divider";
 import Summary from "./components/summary";
 import Skills from "./components/skills";
+import {marked} from 'marked';
+import example from "./Example.md"
 
 function App() {
+    const [md, setMD] = useState<string>("");
+
+    useEffect(() => {
+        fetch(example)
+            .then((response) => response.text())
+            .then((text) => {
+                setMD(text);
+            });
+    }, []);
+
+
+    marked.use({
+        pedantic: false,
+        gfm: true,
+        breaks: true,
+        sanitize: true,
+        smartypants: false,
+        xhtml: false
+    });
+
     return (
         <>
+            <div dangerouslySetInnerHTML={{__html: marked.parse(md)}}/>
             <Page>
                 <Summary/>
                 <Divider title="skills"/>
