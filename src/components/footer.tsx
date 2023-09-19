00915e2f43eb49df05048734bf8e19def389fb14
Fredrik Thune <fredrik.thune@stakater.com>
2023-09-19 12:28:44 +0200

    Adds header and footer to match example resume

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,19 @@ import Box from "./box";
 
 const Footer = () => {
     return (
-        <Box className="footer" borderTop="solid thick #F0CB31" paddingBottom="10mm"/>
+        <Box className="footer" borderTop="solid thick #F0CB31" paddingBottom="3mm" paddingTop="3mm">
+            <Box display="flex" justifyContent="space-around" width="100%">
+                <Box display="flex" alignItems="center">
+                    <img height={20} src={process.env.PUBLIC_URL + "/website.png"}/>&nbsp;&nbsp;&nbsp;<a href="https://stakater.com">https://stakater.com</a>
+                </Box>
+                <Box display="flex" alignItems="center">
+                    <img height={20} src={process.env.PUBLIC_URL + "/linkedin.png"}/>&nbsp;&nbsp;&nbsp;<a href="https://linkedin.com/company/stakater">linkedin.com/company/stakater</a>
+                </Box>
+                <Box display="flex" alignItems="center">
+                    <img height={20} src={process.env.PUBLIC_URL + "/github.png"}/>&nbsp;&nbsp;&nbsp;<a href="https://github.com/stakater">github.com/stakater</a>
+                </Box>
+            </Box>
+        </Box>
     );
 };
 
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,9 +3,24 @@ import Box from "./box";
 
 const Header = () => {
     return (
-        <Box display="inline-flex" className="header">
+        <Box display="flex" justifyContent="space-between" alignItems="center" className="header">
+            {/* Logo aligned to the far left */}
             <Box display="inline-flex">
-                <img height={42} src={process.env.PUBLIC_URL + "/logo.svg"}/>
+                <img height={56} src={process.env.PUBLIC_URL + "/logo.svg"} alt="Logo"/>
+            </Box>
+
+            {/* Group for address and phone number aligned to the far right */}
+            <Box display="flex" alignItems="start" fontSize="0.7em" lineHeight="1.5">
+                <Box flexDirection="column" marginRight={20}>
+                    <Box><b>Address</b></Box>
+                    <Box>Mäster Samuelsgatan 36,</Box>
+                    <Box>111 57 Stockholm, Sweden</Box>
+                </Box>
+
+                <Box flexDirection="column">
+                    <Box><b>Contact</b></Box>
+                    <Box>+46(0)739-458753</Box>
+                </Box>
             </Box>
         </Box>
     );
