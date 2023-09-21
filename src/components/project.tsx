5d57763c174b448fedd0ad3019124254b0667f36
Fredrik Thune <fredrik.thune@stakater.com>
2023-09-21 13:56:08 +0200

    WIP: gray box in project

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -5,8 +5,17 @@ import { IProject } from "../interfaces/resume.interface";
 const Project = ({project}: {project: IProject}) => {
     return (
         <Box margin = "5mm" marginLeft="15mm" marginRight="15mm">
-            <Box>
-                <h3>{project.name} - {project.company} <br/>{project.startDate} → {project.endDate}</h3>
+            <Box display="flex" alignItems="center">
+                 {/*
+                <Box width="20mm" textAlign="center" height="10mm" border-radius="5px" background="linear-gradient(90deg, rgba(90,90,90,1) 0%, rgba(70,70,70,1) 35%, rgba(60,60,60,1) 100%)">
+                    <img height="38mm"
+                        src={ process.env.PUBLIC_URL + "logo192.png" }
+                    />
+                </Box>
+                */}
+                <Box paddingLeft="2mm">
+                    <h3>{project.name} - {project.company} <br/>{project.startDate} → {project.endDate}</h3>
+                </Box>
             </Box>
             <Box>
                 {project.description}
